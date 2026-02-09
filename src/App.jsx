ff6d8ce628d3ba447b1ed9bd1c33635054539db4
Praveen Katti <praveendk25@gmail.com>
2026-02-09 23:43:29 +0530

    correction watch page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
+import WatchLayout from "./layouts/WatchLayout"
 import Home from "./pages/Home";
 import Watch from "./pages/Watch"
 
@@ -12,6 +13,8 @@ export default function App() {
         <Route element={<MainLayout />}>
           {/* Home page */}
           <Route path="/" element={<Home />} />
+        </Route>
+        <Route element={<WatchLayout />}>
           <Route path="/Watch/:videoId" element={<Watch />} />
         </Route>
       </Routes>
